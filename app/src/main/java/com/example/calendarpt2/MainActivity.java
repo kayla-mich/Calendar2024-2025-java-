1b104ec03d187c5d5a4465b7c9dd5a435a9fd4b5
S1753819 <s1753819@online.houstonisd.org>
2024-02-23 11:50:32 -0600

    trying to make the next button to work

--- app/src/main/java/com/example/calendarpt2/MainActivity.java
+++ app/src/main/java/com/example/calendarpt2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calendarpt2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+   private Button continueB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homescreen), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+       /*EdgeToEdge.enable(this);
+       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.homescreen), (v, insets) -> {
+           Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+
+        continueB=(Button)findViewById(R.id.firstbutton);
+        continueB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //when you click the button it switches to the second screen
+            public void onClick(View v) {
+                openSecondScreen();
+            }
         });
+
+        public void openSecondScreen(){
+            Intent intent = new Intent(this, SecondScreen.class);
+            startActivity(intent);
+        }
+
     }
 
 
